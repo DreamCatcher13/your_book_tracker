0557d382e74fece6a6c3de0ed17dc3057a2e0787
DreamCatcher13 <gerey37ng@gmail.com>
2023-08-10 17:51:36 +0300

    created branch for refactoring

---
+++ rootWindow.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox, filedialog, ttk
+import random, os, json
+
+class rootWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        # Adding a title to the window
+        self.wm_title("Test Application")
+
+        # creating a frame and assigning it to container
+        container = tk.Frame(self, height=400, width=600)
+        # specifying the region where the frame is packed in root
+        container.pack(side="top", fill="both", expand=True)
+
+        # configuring the location of the container using grid
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
